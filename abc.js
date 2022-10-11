22e8967570f8885c07c5ba099c3d521a7a2303c8
ankitnegi151 <115225959+ankitnegi151@users.noreply.github.com>
2022-10-11 19:39:45 +0530

    bulidjava.html

---
+++ abc.js
@@ -0,0 +1,13 @@
+
+
+// space, scary, military, romantic, cowboy, fantasy, superhero
+favouriteMovieGenre("superhero")
+
+// watermelon, tomato, banana, orange, avocado, blueberry
+favouriteFruit("orange")
+
+// light, dark
+favouriteMode("dark")
+
+// sharp, soft, round
+favouriteEdgeStyle("round")
